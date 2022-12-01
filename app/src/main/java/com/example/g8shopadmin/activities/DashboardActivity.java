98354e1ced8a7d48ec79d74d734421120f9030fc
20120473 <76527212+duongminhhieu@users.noreply.github.com>
2022-12-01 21:21:23 +0700

    them activity chat

--- app/src/main/java/com/example/g8shopadmin/activities/DashboardActivity.java
+++ app/src/main/java/com/example/g8shopadmin/activities/DashboardActivity.java
@@ -2,18 +2,32 @@ package com.example.g8shopadmin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.g8shopadmin.R;
 import com.example.g8shopadmin.databinding.ActivityDashboardBinding;
+import com.example.g8shopadmin.databinding.ActivitySignInBinding;
+import com.example.g8shopadmin.utilities.PreferenceManager;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private ActivityDashboardBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+    }
+
+    // hàm lắng nghe các sự kiện trong layout
+    private void setListeners() {
+        binding.iconChat.setOnClickListener(view -> {
+//            Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+//            startActivity(intent);
+//            finish();
+        });
     }
 }
