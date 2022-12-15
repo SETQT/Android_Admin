ffbef8d6776c198110c26af6b5d1f2e188666bba
SETQT <tqthai1852@gmail.com>
2022-12-15 16:21:19 +0700

    [update]Them san pham tu admin https://duongminhhieu.atlassian.net/browse/A2G-70?atlOrigin=eyJpIjoiYjMzNmZiMjBiYTJjNDQ3MTgzYzhhY2FlZTg1ZDIxZWEiLCJwIjoiaiJ9

---
+++ app/src/main/java/com/example/g8shopadmin/activities/activity_admin_select_product.java
@@ -0,0 +1,61 @@
+package com.example.g8shopadmin.activities;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.g8shopadmin.R;
+
+public class activity_admin_select_product extends FragmentActivity implements MainCallbacks, View.OnClickListener {
+    FragmentTransaction ft; selectProductFragmentFirst firstFrag; selectProductFragmentSecond secondFrag;
+
+    View icon_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_select_product);
+
+        icon_back = (View) findViewById(R.id.icon_back_select_product);
+        icon_back.setOnClickListener(this);
+
+        ft = getSupportFragmentManager().beginTransaction();
+        firstFrag = selectProductFragmentFirst.newInstance("first-frag");
+        ft.replace(R.id.select_product_fragment_first, firstFrag);
+        ft.commit();
+
+        ft = getSupportFragmentManager().beginTransaction();
+        secondFrag = selectProductFragmentSecond.newInstance("");
+        ft.replace(R.id.select_product_fragment_second, secondFrag);
+        ft.commit();
+    }
+    @Override
+    public void onMsgFromFragToMain(String sender, String strValue) {
+//        Toast.makeText(getApplication(), " MAIN GOT>> " + sender + "\n" + strValue, Toast.LENGTH_LONG).show();
+        if (sender.equals("RED-FRAG")) {
+            try { // forward blue-data to redFragment using its callback method
+                firstFrag.onMsgFromMainToFragment( strValue);
+            }
+            catch (Exception e) { Log.e("ERROR", "onStrFromFragToMain " + e.getMessage()); }
+        }
+        if (sender.equals("BLUE-FRAG")) {
+            try { // forward blue-data to redFragment using its callback method
+                secondFrag.onMsgFromMainToFragment( strValue);
+            }
+            catch (Exception e) { Log.e("ERROR", "onStrFromFragToMain " + e.getMessage()); }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == icon_back.getId()) {
+            Intent moveActivity = new Intent(getApplicationContext(), activity_admin_create_promotions.class);
+            startActivity(moveActivity);
+        }
+
+    }
+}
